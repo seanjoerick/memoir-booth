cf0c961a8dd2d9ec810dbaa411d496fe05f5eee2
Thorfin <130940442+seanjoerick@users.noreply.github.com>
2026-03-17 15:39:44 +0800

    feat: add particles in the landing main

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,8 +1,21 @@
 import Hero from "@/components/landing/hero/Hero";
+import Particles from "@/components/common/particles/Particles";
+import "./Landing.css";
 
 function Landing() {
   return (
-    <main>
+    <main className="landing">
+      <Particles
+        particleColors={["#000000"]}
+        particleCount={300}
+        particleSpread={10}
+        speed={0.1}
+        particleBaseSize={100}
+        moveParticlesOnHover={false}
+        alphaParticles={false}
+        disableRotation={false}
+        pixelRatio={1}
+      />
       <Hero />
     </main>
   );
